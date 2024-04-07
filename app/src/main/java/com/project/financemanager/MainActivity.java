78fa804458a5cea24432d7a1966e4773982425dd
Luan0195266 <luan0195266@huce.edu.vn>
2024-04-07 21:11:57 +0700

    v0.1: Bottom menu...

---
+++ app/src/main/java/com/project/financemanager/MainActivity.java
@@ -0,0 +1,41 @@
+package com.project.financemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+
+import com.project.financemanager.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.wallets) {
+                replaceFragment(new WalletFragment());
+            } else if (item.getItemId() == R.id.charts) {
+                replaceFragment(new ChartFragment());
+            } else if (item.getItemId() == R.id.account) {
+                replaceFragment(new AccountFragment());
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
