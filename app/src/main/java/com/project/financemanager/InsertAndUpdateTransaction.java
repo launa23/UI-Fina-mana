e49443c54ab9a8ff1404c13c9e48e5047cf7e43d
Luan0195266 <luan0195266@huce.edu.vn>
2024-04-17 11:50:01 +0700

    v1.1 & v1.2: Click on transaction...

--- app/src/main/java/com/project/financemanager/InsertAndUpdateTransaction.java
+++ app/src/main/java/com/project/financemanager/InsertAndUpdateTransaction.java
@@ -32,14 +32,14 @@ public class InsertAndUpdateTransaction extends AppCompatActivity {
         inputAmonut = findViewById(R.id.inputAmount);
         txtCategoryName = findViewById(R.id.txtCategoryNameInUpdate);
         imgCategory = findViewById(R.id.imgCategoryInUpdate);
-
-
         autoCompleteTextView = findViewById(R.id.autoCompleteTxt);
+
         autoCompleteTextView.setText(items[0]);
         arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item, items);
-
         autoCompleteTextView.setAdapter(arrayAdapter);
 
+
+        // Click chọn loại transaction trên toolbar
         autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -57,20 +57,24 @@ public class InsertAndUpdateTransaction extends AppCompatActivity {
         fillDataToTransaction();
     }
 
+    // Đổ dữ liệu của transaction vào các trường tương ứng
     private void fillDataToTransaction(){
         Intent intent = getIntent();
         Transaction myObject = (Transaction) intent.getSerializableExtra("transaction");
         if(myObject.getType().equals("Income")){
             inputAmonut.setTextColor(Color.GREEN);
+            autoCompleteTextView.setText(items[1]);
         }
         else {
             inputAmonut.setTextColor(Color.RED);
+            autoCompleteTextView.setText(items[0]);
         }
         inputAmonut.setText(myObject.getAmount());
         txtCategoryName.setText(myObject.getCategoryName());
         txtCategoryName.setTextColor(Color.BLACK);
         int resourceId = this.getResources().getIdentifier(myObject.getImage(), "drawable", this.getPackageName());
         imgCategory.setImageResource(resourceId);
+
         // Còn nữa, làm sau......
 
     }
