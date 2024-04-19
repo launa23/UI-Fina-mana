5f4c9212415b9fc9ec16a138845462d0088bc0f2
Luan0195266 <luan0195266@huce.edu.vn>
2024-04-19 17:14:30 +0700

    v1.4: Update datePicker and timePicker...

--- app/src/main/java/com/project/financemanager/InsertAndUpdateTransaction.java
+++ app/src/main/java/com/project/financemanager/InsertAndUpdateTransaction.java
@@ -4,6 +4,8 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -12,15 +14,18 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.project.financemanager.models.TitleTime;
 import com.project.financemanager.models.Transaction;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class InsertAndUpdateTransaction extends AppCompatActivity {
@@ -34,8 +39,9 @@ public class InsertAndUpdateTransaction extends AppCompatActivity {
     private EditText inputDescription;
     private TextView txtNameWalletInUpdate;
     private TextView txtIdWalletInUpdate;
-
     private RelativeLayout relative5;
+    private TextView txtDateInUpdate;
+    private TextView txtHourInUpdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,11 +55,36 @@ public class InsertAndUpdateTransaction extends AppCompatActivity {
         txtNameWalletInUpdate = findViewById(R.id.txtNameWalletInUpdate);
         relative5 = findViewById(R.id.relative5);
         txtIdWalletInUpdate = findViewById(R.id.txtIdWalletInUpdate);
+        txtDateInUpdate = findViewById(R.id.txtDateInUpdate);
+        txtHourInUpdate = findViewById(R.id.txtHourInUpdate);
+
         autoCompleteTextView.setText(items[0]);
         arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item, items);
         autoCompleteTextView.setAdapter(arrayAdapter);
 
+        Calendar calendar = Calendar.getInstance();
+        int monthCurrent = calendar.get(Calendar.MONTH)+1;
+        int dateCurrent = calendar.get(Calendar.DAY_OF_MONTH);
+        int yearCurrent = calendar.get(Calendar.YEAR);
+        int hourCurrent = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        String strDate = "Hôm nay, " + dateCurrent + "/" + monthCurrent  + "/" + yearCurrent;
+        String strTime = hourCurrent + ":" + minute;
+        txtHourInUpdate.setText(strTime);
+        txtDateInUpdate.setText(strDate);
+        txtDateInUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onDialogDate();
+            }
+        });
 
+        txtHourInUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onDialogTime();
+            }
+        });
         // Click chọn loại transaction trên toolbar
         autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -117,6 +148,46 @@ public class InsertAndUpdateTransaction extends AppCompatActivity {
         txtNameWalletInUpdate.setTextColor(Color.BLACK);
         txtNameWalletInUpdate.setText(myObject.getWalletName());
         // Còn nữa, làm sau......
+    }
 
+    private void onDialogDate(){
+        Calendar calendar = Calendar.getInstance();
+        int monthCurrent = calendar.get(Calendar.MONTH)+1;
+        int dateCurrent = calendar.get(Calendar.DAY_OF_MONTH);
+        int yearCurrent = calendar.get(Calendar.YEAR);
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String strDate;
+                if(dateCurrent == dayOfMonth && monthCurrent == month+1 && yearCurrent == year){
+                    strDate = "Hôm nay, " + dayOfMonth + "/" + (month+1) + "/" + year;
+                }
+                else if (dateCurrent - 1 == dayOfMonth && monthCurrent == month+1 &&yearCurrent == year){
+                    strDate = "Hôm qua, " + dayOfMonth + "/" + (month+1) + "/" + year;
+                }
+                else if (dateCurrent + 1 == dayOfMonth && monthCurrent == month+1 &&yearCurrent == year){
+                    strDate = "Ngày mai, " + dayOfMonth + "/" + (month+1) + "/" + year;
+                }
+                else {
+                    strDate = dayOfMonth + "/" + (month+1) + "/" + year;
+                }
+                txtDateInUpdate.setText(strDate);
+            }
+        }, yearCurrent, monthCurrent-1, dateCurrent);
+        dialog.show();
+    }
+
+    private void onDialogTime(){
+        Calendar calendar = Calendar.getInstance();
+        int hourCurrent = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog dialog = new TimePickerDialog(this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar_MinWidth,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        txtHourInUpdate.setText(hourOfDay + ":" + minute);
+                    }
+        }, hourCurrent, minute, true);
+        dialog.show();
     }
 }
